c4b3bf46c2929dd7e9171d8218f31556ac6fc7b0
Akshaya Ma <akshai2288@gmail.com>
2016-08-08 15:36:38 +0530

    Add files via upload

---
+++ Palindromes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Palindromes {
+  
+  
+  public static void main(String[] args) {
+    int n,an=0,a=0,t,i=0,t1;
+    
+    
+    Scanner s1=new Scanner(System.in);
+    n=s1.nextInt();
+    while(n>0)
+    {
+      t=n%10;
+      a=a*10+(t*t*t);
+      n=n/10;
+    }
+    if(a==n)
+    {
+      System.out.println("palindrome");
+    }
+    else
+    {
+      System.out.println("not palindrome");  
+    }
+    
+  }
+  }
